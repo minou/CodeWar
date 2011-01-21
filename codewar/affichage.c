ac34182c0565f26bb09ce93462ad05cb35eb7b59
minou <niemczycki.nicolas@gmail.com>
2011-01-21 23:12:06 +0100

    Commencement de la méthode affiche_plateau qui permettera d'afficher la phateau

---
+++ codewar/affichage.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define NB_LIGNES 16
+#define NB_COLONNES 16
+
+/*Méthode permettant d'afficher le plateau */
+void affiche_plateau(){
+    int ligne, colonne;
+
+    for (ligne = 0, ligne < NB_LIGNES, ligne++)
+    {
+        for (colonne = 0, colonne < NB_COLONNES, colonne++)
+        {
+            
+        }
+    }
+}
