00e96b0415fdd0fe031bf4923f30ad59666d2886
nico <niemczycki.nicolas@gmail.com>
2011-01-24 23:15:03 +0100

    Continuation de la méthode display_grid et ajout de free

--- codewar/display.c
+++ codewar/display.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "struct.h"
 
-#define NB_LIGNES 16
-#define NB_COLONNES 16
+#define NB_LINE 16
+#define NB_COLUMN 16
+#define NB_MALLOC 18
+#define SIZE 40
 
 /* Méthode permettant d'afficher le plateau */
 void display_grid(CPU grid[256])
 {
-    int ligne, colonne;
-    int nb_lignes = NB_LIGNES;
-    int nb_colonnes = NB_COLONNES;
+    int i = 0;
+    int nb_malloc = NB_MALLOC;
+    int size = SIZE;
 
-    for (ligne = 0; ligne < nb_lignes; ligne++)
+    char ** line = malloc(nb_malloc * sizeof(char *));
+    
+    /* Init */
+    while (i < nb_malloc)
     {
-        for (colonne = 0; colonne < nb_colonnes; colonne++)
-        {
-        }
-        if (ligne < 10)
-        {
-            printf("%d  ", ligne);
-        }
-        else
-        {
-            printf("%d ", ligne);
-        }
-        printf("\n");
+        line[i] = malloc(size);
+        i++;
     }
+
+    line[0] = "   +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+";
+
+    i = 1;
+    while (i < nb_malloc - 1)
+    {
+        sprintf(line[i], "%d", i-1); 
+        i++;
+    }
+
+
+    i = 1;
+    /* Display */
+    while (i < nb_malloc)
+    {
+        printf("%s\n", line[0]);
+        printf("%s\n", line[i]);
+        i++;
+    }
+
+    free2(line);
+}
+
+void free2(char ** line){
+    int i = 1;
+    int nb_malloc = NB_MALLOC;
+    
+    while (i < nb_malloc)
+    {
+        free(line[i]);
+        i++;
+    }
+    free(line);
 }
