35d7e0563e3e66462b0361b2ae651864e0a9e914
bastien <bastien@bastien-laptop.(none)>
2011-01-25 22:06:46 +0100

    Voici le premier jet du code tentative de définition des structures et de l'affichage commmenter en anglais

--- codewar/test.c
+++ codewar/test.c
@@ -28,34 +28,42 @@ int c;/*carry flag, =1 if a op generates a carry*/
 int z;/*zero flag, =1 if the result of an op is = 0*/
 int n;/*negativity flag, =1 if the result of an op is < 0*/
 
-char l [17] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-
+char l [16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 /*affichage de l'état de la mémoire du CPU*/
-/*void displayCPU(){
+void displayCPU(){
 	int i,j,k;
 	int w = 0;
-	for(i = 0; i < 18; i++){
-		
-		for(j = 0; j<18; j++){
-			printf("%5d", &mem[i]);
+	for(i = 0; i < 16; i++){
+		printf("|  0x%c0  |",l[i]);
+		for(j = 0; j<16; j++){
+			printf("%4c%c",l[0],l[0] );
 			if(j == 15){
+				printf("  |");
 				printf("\n");
 			}
 		}
 	}
+	printf("---------+-----------------------------------------------------------------------------------");
 	printf("\n");
-}*/
+}
 void display(){
 	int k;
-	for(k = 0; k<18; k++){
-		printf("%d",l[k]);
-		printf("\n");
+	printf("---------+-----------------------------------------------------------------------------------");
+	printf("\n");
+	printf("|        |");
+	for(k = 0; k<16; k++){
+		printf("%5c",l[k]);	
 	}
-	
+	printf("  |");
+	printf("\n");
+	printf("---------+-----------------------------------------------------------------------------------");
+	printf("\n");
 }
 int main(){
-		display();
-		return 0;
+	
+    display();
+    displayCPU();
+    return 0;
 }
 
 		
