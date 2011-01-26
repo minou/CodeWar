eee47dc046b662aede442fabdf5974586000beab
nico <niemczycki.nicolas@gmail.com>
2011-01-26 22:50:52 +0100

    Plus que un fichier main.c

--- codewar/main.c
+++ codewar/main.c
@@ -4,5 +4,15 @@ int main(int args , char ** argv)
 {
     CPU grid[256];
     display_grid(grid);
+
+    setMem();
+    displayCPU();
+    setReg();
+    displayReg();
+
+    char ** stock_hexa;
+    openFile(argv[1], stock_hexa);
+    //printf("%s\n", stock_hexa[0]);
+
     return 0;
 }
--- codewar/open.c
+++ codewar/open.c
@@ -3,29 +3,30 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define SIZE_BUFFER 16
+
 int openFile(char * file, char ** stock_hexa){
-      int fd;
-      unsigned char buffer[16];
-      fd = open(file, O_RDONLY);
-      int r;
-      while ((r =read(fd, buffer, 16)) != 0){
-	    int i;	
-	    for (i=0; i<r; i++) {
-		  //stock_hexa[i] = buffer[i];
-		  printf("%02x\n", buffer[i]);
-		  //printf("%02x\n", stock_hexa[i]);
-	    }
-      }
-      return 0;
+    int i, fd, r;
+    unsigned char buffer[SIZE_BUFFER];
+    fd = open(file, O_RDONLY);
+    if (fd == -1){
+        perror("open");
+        return -1;
+    }
+    while ((r = read(fd, buffer, SIZE_BUFFER)) != 0){
+        if (r == -1){
+            perror("read");
+            return -1;
+        }
+        for (i = 0; i < r; i++){
+            //stock_hexa[i] = buffer[i];
+            printf("%02x\n", buffer[i]);
+            //printf("%02x\n", stock_hexa[i]);
+        }
+    }
+    return 0;
 }
-      
+
 int reverse(char ** buffer){
-      return 0;
-}
-      
-int main(int argc, char ** argv){
-      char ** stock_hexa;
-      openFile(argv[1], stock_hexa);
-      //printf("%s\n", stock_hexa[0]);
-      return 0;
+    return 0;
 }
