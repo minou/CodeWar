4a520c0a1235631af70235edd865e7ebe7dffa6c
Nicolas.NIEMCZYCKI <niemczyn@epicea11.(none)>
2011-01-28 13:25:40 +0100

    Init RAM

--- codewar/main.c
+++ codewar/main.c
@@ -8,6 +8,15 @@ int main(int args , char ** argv)
     char * option = argv[1];
 
     CPU grid[NB_CPU];
+    int i = 0;
+    /* Init CPU */
+    unsigned char RAM[MEM_SIZE];
+
+    while (i < NB_CPU){
+        grid[i].RAM = RAM;
+        i++;
+    }
+
     unsigned char ** stock_hexa = malloc(15 * sizeof(unsigned char *));
 
     if (option == NULL){
