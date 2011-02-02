9919e39978afa2c9209a74ff2ed6565752a5a8a8
Bastien.OUTTERYCK <outteryb@frene09.(none)>
2011-02-02 13:45:24 +0100

    update

--- codewar/open_load.c
+++ codewar/open_load.c
@@ -7,49 +7,75 @@
 #include <stdlib.h>
 
 /*this function open the file put in parameter and store it in the CPU memory 
- * return 0 if everything went smoothly
- * return -1 if there is an error
- */
-int open_load_File(char * file, unsigned char ** stock_hexa){
-    int fd, r, i = 0;
-    unsigned char * buffer = malloc(HEXA);
-
-    fd = open(file, O_RDONLY);
-    if (fd == -1){
-        perror("open_in_open_load");
-        return -1;
-    }
-    /*we want to read 200bits and then store it in the CPU
+* return 0 if everything went smoothly
+* return -1 if there is an error
+*/
+int open_load_File(char * file, CPU cpu){
+     int fd, r, i = 0;
+     unsigned char * buffer = malloc(HEXA);
+     
+     fd = open(file, O_RDONLY);
+     if (fd == -1){
+	  perror("open_in_open_load");
+	  return -1;
+     }
+     /*we want to read 200bits and then store it in the CPU
      * the first 16octets are reserved for the the registers
      * so we start at 17 (in the CPU) to load the program
      * this means that the program cannot weight more than 240octets???
-     * we need to know the lenght of the file 
+     * we need to know the length of the file 
      */
-    while ( (r = read(fd, buffer, HEXA)) != 0){
-        if (r == -1){
-            perror("read_in_open_load");
-            return -1;
-        }
-        printf("ligne 33\n");
-        if (r > 0){
-            /*stock_hexa[i] =*/
-            printf("%02x\n", buffer);/*store the 16 first octets in the first line*/
-            i++;
-            /*printf("%d\n", i);*/
-        }
-        printf("%d\n", r);
-        printf("%d\n",i);
-    }
-    return 0;
-}
+     while ( (r = read(fd, buffer, HEXA)) != 0){
+	  if (r == -1){
+	       perror("read_in_open_load");
+	       return -1;
+	  }
+	  if (r > 0){
+            cpu.RAM[i] = buffer[0];/*store octet by octet in the memory*/
+	    i++;
+	  }
 
-int main(int args , char ** argv){
-    int i;
-    char * file = "output";
-    unsigned char ** stock_hexa;
-    open_load_File(file, stock_hexa);
-    for (i = 0; i<HEXA; i++){
-        printf("%s\n", stock_hexa[i]);
-    }
-    return 0;
+     }
+     return 0;
 }
+int get_file_size(char *file_name){
+    FILE *fp;
+    int file_size;
+
+    file_size = 0;
+    if ((fp = fopen(file_name, "rb" )) == NULL) {
+	      fprintf(stderr, "Cannot open %s.\n", file_name);
+       return(file_size);
+    }
+    if (fseek(fp, (long)(0), SEEK_END) != 0) {
+       fclose(fp);
+       return(file_size);
+    }
+
+    file_size = (int) (ftell(fp));
+    fclose(fp);
+
+    return(file_size);
+ }
+     int main(int args , char ** argv){
+	  char * file = "output";
+	  CPU cpu;	
+	  int i ;
+	  open_load_File(file, cpu);
+
+	  /*for (i = 0; i<MEM_SIZE; i++){
+	       printf("%02x", cpu.RAM[i]);
+	       if( (i%15) == 0){
+		   printf("\n"); 
+	       }
+	  }*/
+	  displayCPU(cpu);
+	  printf("\n");
+	  return 0;
+	  
+     }
+     
+  
+  
+  
+	
