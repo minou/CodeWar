6a06925460125672a6e472a1cb3fe28595beb1f3
nico <niemczycki.nicolas@gmail.com>
2011-02-09 19:39:28 +0100

    Mise à jour des structures

--- codewar/extract.c
+++ codewar/extract.c
@@ -23,3 +23,40 @@ void decryte(char *s){}
 void next(CPU * cpu){
     /* Load register R6 or PC */
 }
+
+
+instruction insts[] = {
+    {"MOVE", 2, NULL, NULL, NULL},
+    {"PUSH", 1, NULL, NULL, NULL},
+    {"ADD", 2, NULL, NULL, NULL},
+    {"CMP", 2, NULL, NULL, NULL},
+    {"SUB", 2, NULL, NULL, NULL},
+    {"LSL", 2, NULL, NULL, NULL},
+    {"LSR", 2, NULL, NULL, NULL},
+    {"AND", 2, NULL, NULL, NULL},
+    {"OR", 2, NULL, NULL, NULL},
+    {"XOR", 2, NULL, NULL, NULL},
+    {"NOT", 1, NULL, NULL, NULL},
+    {"BCC", 1, NULL, NULL, NULL},
+    {"BGT", 1, NULL, NULL, NULL},
+    {"BCS", 1, NULL, NULL, NULL},
+    {"BLT", 1, NULL, NULL, NULL},
+    {"BEQ", 1, NULL, NULL, NULL},
+    {"BNE", 1, NULL, NULL, NULL},
+    {"BLE", 1, NULL, NULL, NULL},
+    {"BGE", 1, NULL, NULL, NULL},
+    {"BRA", 1, NULL, NULL, NULL},
+    {"BSR", 1, NULL, NULL, NULL},
+    {"JCC/JGT", 1, NULL, NULL, NULL},
+    {"JCS", 1, NULL, NULL, NULL},
+    {"JLT", 1, NULL, NULL, NULL},
+    {"JEQ", 1, NULL, NULL, NULL},
+    {"JNE", 1, NULL, NULL, NULL},
+    {"JLE", 1, NULL, NULL, NULL},
+    {"JGE", 1, NULL, NULL, NULL},
+    {"JMP", 1, NULL, NULL, NULL},
+    {"JSR", 1, NULL, NULL, NULL},
+    {"RTS", 0, NULL, NULL, NULL},
+    {"TRAP", 1, NULL, NULL, NULL},
+    {"RTE", 0, NULL, NULL, NULL},
+};
--- codewar/struct.h
+++ codewar/struct.h
@@ -19,80 +19,42 @@ typedef struct CPU
 {
     unsigned char RAM[MEM_SIZE];
     word registers[REG];
-     /*CPU flags, indicates his state*/
-    word stateregister[1];
-    /*carry flag, =1 if a op generates a carry*/
-    /*zero flag, =1 if the result of an op is = 0*/
-    /*negativity flag, =1 if the result of an op is < 0*/
+    /*CPU flags, indicates his state*/
+    int c;/*carry flag, =1 if a op generates a carry*/
+    int z;/*zero flag, =1 if the result of an op is = 0*/
+    int n;/*negativity flag, =1 if the result of an op is < 0*/
 } CPU;
 
 typedef word color;
-/*in this structure we store the instruction in binary*/
+
 typedef struct binary{
       int tab [16];
       
 }binary;
 
 enum regist{
-    RO;
-    R1;
-    R2;
-    R3;
-    R4;
-    R4;
-    R5;
-    R6;
-    PC = R6;
-    R7;
-    SP = R7;
-}
+    RO,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    PC = R6,
+    R7,
+    SP = R7
+};
 
-typedef struct instruction{
-    char name[10];
-    int nbOp;
-    void (*executeZeroOp());
-    void (*executeOneOp(operande));
-    void (*executeTwoOp(operande, operande));
-}instruction;
 
 typedef struct operande{
     int type;
     int value;
 } operande;
 
-instruction insts[]{
-    {"MOVE", 2, NULL, NULL, move};
-    {"PUSH", 1, NULL, push, NULL};
-    {"ADD", 2, NULL, NULL, add};
-    {"CMP", 2, NULL, NULL, cmp};
-    {"SUB", 2, NULL, NULL, sub};
-    {"LSL", 2, NULL, NULL, lsl};
-    {"LSR", 2, NULL, NULL, lsr};
-    {"AND", 2, NULL, NULL, and};
-    {"OR", 2, NULL, NULL, or};
-    {"XOR", 2, NULL, NULL, xor};
-    {"NOT", 1, NULL, not, NULL};
-    {"BCC", 1, NULL, bcc, NULL};
-    /*{"BGT", 1, NULL, bgt, NULL};*/
-    {"BCS", 1, NULL, bcs, NULL};
-    /*{"BLT", 1, NULL, blt, NULL};*/
-    {"BEQ", 1, NULL, beq, NULL};
-    {"BNE", 1, NULL, bne, NULL};
-    {"BLE", 1, NULL, ble, NULL};
-    {"BGE", 1, NULL, bge, NULL};
-    {"BRA", 1, NULL, bra, NULL};
-    {"BSR", 1, NULL, bsr, NULL};
-    {"JCC", 1, NULL, jcc, NULL};
-    /*{"JGT", 1, NULL, jgt, NULL};*/
-    {"JCS", 1, NULL, jcs, NULL};
-    /*{"JLT", 1, NULL, jlt, NULL};*/
-    {"JEQ", 1, NULL, jeq, NULL};
-    {"JNE", 1, NULL, jne, NULL};
-    {"JLE", 1, NULL, jle, NULL};
-    {"JGE", 1, NULL, jge, NULL};
-    {"JMP", 1, NULL, jmp, NULL};
-    {"JSR", 1, NULL, jsr, NULL};
-    {"RTS", 0, NULL, rts, NULL};
-    {"TRAP", 1, NULL, trap, NULL};
-    {"RTE", 0, NULL, rte, NULL};
-}
+typedef struct instruction{
+    char name[10];
+    int nbOp;
+    void (*executeZeroOp)();
+    void (*executeOneOp)(operande);
+    void (*executeTwoOp)(operande, operande);
+}instruction;
