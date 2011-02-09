5c114156d249cf195df493a6d430a7fc4f09cb7e
bastoss <tortue@fdk.com>
2011-02-09 16:18:58 +0100

    BinaryToHex

--- codewar/conversion.c
+++ codewar/conversion.c
@@ -2,6 +2,7 @@
 #include "struct.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /*the word has already been switch*/
 binary * conversion(word w, binary * result){
@@ -9,9 +10,23 @@ binary * conversion(word w, binary * result){
      for(i = 0; i<=1 ; i++){
 	  reste = w.word[i];
 	  for (j=0;j<8;j++){
-	       result->tab[i][7 - j] = reste%2;
+		
+	       result->tab[15 - ((7 * i) + j)] = reste%2;
 	       reste = reste/2; 
 	  }
      }
      return result;    
+}
+/*this function convert the first 5 bites in hexadecimal
+* to determine the instruction
+*/
+unsigned char BinaryToHexa(binary * bin){
+      int i;
+      int nb = 0;
+      unsigned char result;
+      for (i = 0; i<5; i++){
+	    nb+=bin[4-i]*int( pow(2,i) );
+      }
+      result = nb;
+      return result;
 }
