265c04d6a6aceb3fbd20e3ccdfd11d1fbd608edb
nico <niemczycki.nicolas@gmail.com>
2011-02-15 20:21:58 +0100

    Ajout de méthode pour gérer le type2, la value et la value2 de operande

--- codewar/conversion.c
+++ codewar/conversion.c
@@ -43,3 +43,39 @@ unsigned char instructionType(binary * bin){
     result = nb;
     return result;
 }
+
+unsigned char instructionType2(binary * bin){
+    int i = 0;
+    int nb = 0;
+    unsigned char result;
+    while (i < 3){
+        nb += bin->tab[2 + 8 - i] * (int)pow(i, 2);
+        i++;
+    }
+    result = nb;
+    return result;
+}
+
+unsigned char instructionValue(binary * bin){
+    int i = 0;
+    int nb = 0;
+    unsigned char result;
+    while (i < 8){
+        nb += bin->tab[7 + 8 - i] * (int)pow(i, 2);
+        i++;
+    }
+    result = nb;
+    return result;
+}
+
+unsigned char instructionValue2(binary * bin){
+    int i = 0;
+    int nb = 0;
+    unsigned char result;
+    while (i < 5){
+        nb += bin->tab[4 + 11 - i] * (int)pow(i, 2);
+        i++;
+    }
+    result = nb;
+    return result;
+}
