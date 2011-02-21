e334091cd9a9753c048ac371f54baf8282d9525b
nico <nico@nico-Studio-1749.(none)>
2011-02-22 00:20:22 +0100

    Déplacement des méthodes dans extract.c

--- codewar/extract.c
+++ codewar/extract.c
@@ -12,3 +12,30 @@ word extract(CPU * cpu){
     cpu->registers[6]+=2;
     return wrd;
 }
+
+/* an instruction is coded on 16 bits -> 2 octets -> 2 words */
+/*the word has already been switch*/
+
+/* this function convert the first 5 bites in hexadecimal
+ *  to determine the instruction
+ */
+word instructionName(word * wrd){
+     printf("code l'instruction %05x\n",(*wrd >> 11));
+    return *wrd >> 11;
+}
+
+word instructionType(word wrd){
+    return (wrd << 5) >> 13;
+}
+
+word instructionType2(word wrd){
+    return (wrd << 8) >> 13;
+}
+
+word instructionValue(word wrd){
+    return (wrd & 8);
+}
+
+word instructionValue2(word wrd){
+    return (wrd & 5);
+}
