7f3605c34bed402b124502a98cd0386df8fc83a9
bastoss <tortue@fdk.com>
2011-03-02 16:07:10 +0100

    move partiel

--- codewar/instruction.c
+++ codewar/instruction.c
@@ -1,61 +1,37 @@
+#include "struct.h"
+#include "prototype.h"
 
-/*JE PENSE QU'IL FAUT REVOIR LES METHODES D'EXTRACT.C, MAIS J'AI DES PROBLEMES 
-  AVEC LES ">>" ET LES "<<"
-*/
-
-/*how do we access the 2 word of the move???*/
-void move(CPU * cpu, operande op1, operande op2){
-      if (op1.type >= 0 && op1.type =<3){
-	    if ((op2.type >= 0 && op2.type =<3) || op2.type == 5){
-	    }
-      }
-      else if (op1.type == 4 || op1.type == 5){/*cas où t1 est une adresse ou une valeur immediate*/ 
+void move(CPU * cpu, operande op1, operande op2){     
+      if (op1.type == 4 || op1.type == 5){/*cas où t1 est une adresse ou une valeur immediate*/ 
 	    if (op2.type >= 0 && op2.type =<3){
-		  /* On échange les value pour les mettre dans les bonnes operandes */
+		  /*alors rrr(op1.value) codent le numero de registre(value) de la destination(op2)*/
 		  int value = op1.value;
 		  op1.value = op2.value;
-		  op2.value = op1.value;
+		  op2.value = value;
+		 
+		  value = get_value(op1); /* On récupère la valeur immédiate ou une adresse (v) */
 		  
-		  /* On récupère la value de la valeur immédiate ou une adresse (v) */
-		  value = get_value(op1);
 		  if (op2.type == 0){
 			cpu->registers[op2.value] = value;
 		  }
+	    }
+      }
+      else if (op1.type >= 0 && op1.type =<3){ /*Sinon rrr(op1.value) codent le numero de registre de la source soit (op1.value)*/
+	    if ((op2.type >= 0 && op2.type =<3) || op2.type == 5){/*alors (v) le deuxieme mot (op2.value) code la valeur de destination*/
+		  if (op1.type == 0){/*si adressage direct pour la source*/
+			if(op2.type == 0){/*si adressage direct pour la destination*/
+			      /*cas du move de registre direct à registre direct*/
+			      cpu->registers[op2.value] = cpu->registers[op1.value];
+			}
+			if(op2.type == 5){/*si la destination est une adresse*/
+			    /*op2.value c'est l'adresse mais pas la vrai, accès réel impossible*/  
+			}
 	    }
       }
       else {
 	    printf("Erreur au niveau des types");
 	    return;
       }    
-     /*if the first operande is an direct value or an adress*/
-     if( (op1.type == 4) || (op1.type == 5) ){
-     /*then the last third bits of the word code the destination register's number*/
-     
-	  /* !!!!!!!!  PROBLEME: difference entre rrr (de la fin du premier mot) et op1.value     ????*/
-	  cpu.registers[op1.value] = ;
-     }
-     else{
-	   /*  if the source is not a direct value or an adress (op1.type != 4) && (op1.type != 5) 
-	       they code the source register's number
-	       then the 2 word codes the destination value
-	       the possibles values are:
-	       a number between 0 and 7
-	       an adress on 16 bits
-	  */
-	   /*case where the second word codes a register's number*/
-	   if(w2 >= 0 && w2<=7){
-	       cpu.registers[w2] = cpu.registers[op1.value];
-	   }
-	   /*case where the second word codes an address*/
-	   else{
-	       *w2 = cpu.registers[op1.value];/*on veut la variable qui se trouve à cette adresse*/
-	   }
-     }
-     /*il faut egalement implementer MOVE.l et MOVE.h
-       gerer les modes d'adressage !?
-       et gerer le registre d'état
-    */         
-}
 
 int get_value(CPU * cpu, operande op){
       /* Registre ok*/
