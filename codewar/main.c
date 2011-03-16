e61390de39b844a993b2a70486b0485301519ffc
Nicolas.NIEMCZYCKI <niemczyn@bouleau10.(none)>
2011-03-16 14:28:44 +0100

    Maj Main

--- codewar/main.c
+++ codewar/main.c
@@ -7,10 +7,9 @@ int main(int args , char ** argv)
     char * option;
     CPU  grid [256];
     char * file;
-    int cpu_num;
     CPU * cpu;
     color color_cpu;
-    int i = 1;
+    int i = 2;
 
     option = argv[1];
     if (option == NULL){
@@ -23,19 +22,24 @@ int main(int args , char ** argv)
         /* Open and load program in CPU */
         /*load automatiquement pour le momment*/
         file = argv[i];
-        cpu_num = randomCPU();
+        cpu.id = randomCPU();
         cpu = &grid[cpu_num];
         color_cpu = randomColor();
         setColor(cpu, color_cpu);
         open_load_File(file, cpu);
         setStack(cpu);
-        printf("Program %s loaded in cpu %d (color %04x)\n", file, cpu_num, color_cpu);
+        printf("Program %s loaded in cpu %d (color %04x)\n", file, cpu.id, color_cpu);
         i++;
     }
-    while (1) {
-        display_prompt();
-        run_commande(grid);
+    if (args[1] == "-p"){
+        while (1) {
+            display_prompt();
+            run_commande(grid);
+        }
     }
-
+    else if (args[1] == "-c"){
+        int nb = args[2];
+        /*i = 3;*/
+    }
     return 0;
 }
